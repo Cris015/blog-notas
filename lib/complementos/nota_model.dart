09b02c25578877f26db106c4ab6e04cd3e788e49
Cristian Castillo <ccpcastillo015@gmail.com>
2025-03-22 16:48:11 -0400

    agregando localStorage

--- lib/complementos/nota_model.dart
+++ lib/complementos/nota_model.dart
@@ -3,4 +3,15 @@ class Nota {
   String contenido;
 
   Nota({required this.titulo, required this.contenido});
+
+  Map<String, dynamic> toJson() {
+    return {'titulo': titulo, 'contenido': contenido};
+  }
+
+  factory Nota.fromJson(Map<String, dynamic> json) {
+    return Nota(
+      titulo: json['titulo'],
+      contenido: json['contenido'],
+    );
+  }
 }
--- lib/complementos/notas_provider.dart
+++ lib/complementos/notas_provider.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:notas/complementos/nota_model.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotasProvider with ChangeNotifier{
   final List<Nota> _notas = [];
 
   List<Nota> get notas => _notas;
 
+  NotasProvider() {
+    _cargarNotas();
+  }
+
   void agregarNota(String titulo, String contenido){
     _notas.add(Nota(titulo: titulo, contenido: contenido));
+    _guardarNotas();
     notifyListeners();
   }
 
   void eliminarNota(int index){
     if(index >= 0 && index < _notas.length){
       _notas.removeAt(index);
+      _guardarNotas();
       notifyListeners();
     }
+  }
+  Future<void> _guardarNotas() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> notasJson = _notas.map((nota) => jsonEncode(nota.toJson())).toList();
+    await prefs.setStringList('notas', notasJson);
+  }
+
+  Future<void> _cargarNotas() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String>? notasJson = prefs.getStringList('notas');
+    if (notasJson != null) {
+      _notas.addAll(notasJson.map((nota) => Nota.fromJson(jsonDecode(nota))));
+    }
+    notifyListeners();
   }
 }
